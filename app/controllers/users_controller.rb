08fa23f7e36b19c05a9eb7cb43e7268d6189a81b
vagrant <vagrant@localhost.localdomain>
2020-06-20 04:54:41 +0000

    スワイプ画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+	def index
+	#ログインユーザー以外のすべてのユーザーを表示
+    @users = User.where.not(id: current_user.id)
+    end
+
 	def show
 		@user = User.find_by(id: params[:id])
 	end
